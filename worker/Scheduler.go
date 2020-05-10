8cb1eb4f3ad10c4e0166e2b146ac0755f38c6529
于洋 <yy_dgs@126.com>
2020-05-10 10:47:24 +0800

    调度任务和项目结构

--- worker/Scheduler.go
+++ worker/Scheduler.go
@@ -1,6 +1,10 @@
 package worker
 
-import "CrontabDemo/common"
+import (
+	"CrontabDemo/common"
+	"fmt"
+	"time"
+)
 
 type Scheduler struct {
 	jobEventChan chan *common.JobEvent             //事件队列
@@ -9,32 +13,88 @@ type Scheduler struct {
 
 var G_Scheduler *Scheduler
 
-func initScheduler() {
+func InitScheduler() (err error) {
 	G_Scheduler = &Scheduler{
 		jobEventChan: make(chan *common.JobEvent, 1000),
+		jobPlanTable: make(map[string]*common.JobSchdulePlan),
 	}
 	go G_Scheduler.schedulerLoop()
+	return nil
+}
+
+//重新计算任务调度状态
+func (scheduler *Scheduler) TrySchedule() (scheduleAfter time.Duration) {
+	var (
+		jobPlan  *common.JobSchdulePlan
+		nearTime *time.Time
+	)
+
+	if len(scheduler.jobPlanTable) == 0 {
+		scheduleAfter = 1 * time.Second
+		return
+	}
+
+	now := time.Now()
+	//遍历所有任务
+	for _, jobPlan = range scheduler.jobPlanTable {
+		if jobPlan.NextTime.Before(now) || jobPlan.NextTime.Equal(now) {
+			//TODO:尝试执行任务
+			fmt.Println("执行任务：" + jobPlan.Job.Name)
+			jobPlan.NextTime = jobPlan.Expr.Next(now)
+		}
+
+		//统计最近一个要执行的任务到期时间
+		if nearTime == nil || jobPlan.NextTime.Before(*nearTime) {
+			nearTime = &jobPlan.NextTime
+		}
+	}
+	//过期的任务立即执行
+
+	//统计最近要过期的任务时间
+	scheduleAfter = (*nearTime).Sub(now)
 	return
 }
 
 //处理任务事件
 func (scheduler *Scheduler) handleJobEvent(jobEvent *common.JobEvent) {
+	var (
+		jobSchedulePlan *common.JobSchdulePlan
+		jobExited       bool
+		err             error
+	)
 	switch jobEvent.EventType {
 	case common.JOB_EVENT_SAVE:
-	case common.JOB_EVENT_DELETE:
+		if jobSchedulePlan, err = common.BuildJobSchedulePlan(jobEvent.Job); err != nil {
+			return
+		}
+		scheduler.jobPlanTable[jobEvent.Job.Name] = jobSchedulePlan
 
+	case common.JOB_EVENT_DELETE:
+		if jobSchedulePlan, jobExited = scheduler.jobPlanTable[jobEvent.Job.Name]; jobExited {
+			delete(scheduler.jobPlanTable, jobEvent.Job.Name)
+		}
 	}
 }
 
 func (scheduler *Scheduler) schedulerLoop() {
 	var (
-		jobEvent *common.JobEvent
+		jobEvent      *common.JobEvent
+		scheduleAfter time.Duration
+		scheduleTimer *time.Timer
 	)
+	scheduleAfter = scheduler.TrySchedule()
+
+	scheduleTimer = time.NewTimer(scheduleAfter)
+
 	for {
 		select {
 		case jobEvent = <-scheduler.jobEventChan:
 			scheduler.handleJobEvent(jobEvent)
+		case <-scheduleTimer.C:
+
 		}
+		scheduleAfter = scheduler.TrySchedule()
+		scheduleTimer.Reset(scheduleAfter)
 	}
 }
 
